5e21518240f07fed8e0b81ff601e572e4ea43fb5
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-08 17:42:16 -0300

    variaveis

---
+++ variaveis.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    //string
+    $nome = 'Jose';
+
+    //int
+    $idade = 29;
+
+    // float
+    $peso = 82.5;
+
+    // boolean true ou false
+    $fumante = false;
+
+    ?>
+
+
+    <h1>Fica cadastral</h1>
+    <br>
+    <p>Nome: <?= $nome ?></p>
+    <p>Idade: <?= $idade ?></p>
+    <p>Peso: <?= $peso ?></p>
+    <p>Fumante: <?= $fumante ?></p>
+
+</body>
+
+</html>
