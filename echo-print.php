8a2a73d4f9f0a2b8d160c27eb7eee26b7d3a175a
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-08 17:14:14 -0300

    inicio

---
+++ echo-print.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+    echo '<strong>Comando</strong> echo<br>';
+
+    echo print('Comando print<hr/>');
+
+    ?>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+    echo 'Utilizando a tag padrão';
+    ?>
+    <br>
+    <?= 'Utilizando a tag impressão' ?>
+    <br>
+    <? echo 'Utilizando a tag curta' ?>
+    <br>
+    <?php
+    echo 'Uso';
+    ?>
+</body>
+
+</html>
