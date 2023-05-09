da8fa2e6d2fd553ee57e89e1d59504c9c0abab7f
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-08 21:18:33 -0300

    if-else

---
+++ if-else.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+
+    <?php
+    // == -> igual
+    // === -> identico é verifica se ambos são string ou int 2 === '2' -> false
+    // != ou <> -> é diferente
+    // !== -> é diferente e verifica se ambos são string ou int ex 5 !== '5' -> verdadeiro
+    // < -> é menor -> 2 < 18 = true
+    // > -> é maior ~~ 5 > 18 = falso
+    // <= Menor ou igual
+    // >= Maior ou igual
+    if (5 >= 5) {
+        echo 'Verdadeiro';
+    } else {
+        echo 'Falso';
+    }
+
+
+    ?>
+
+
+</body>
+
+</html>
