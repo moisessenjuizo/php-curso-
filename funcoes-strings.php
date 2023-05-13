dc7242a158d3f4ced44d3807f797f854983255bd
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-13 15:37:52 -0300

    funcoes

--- funcoes-strings.php
+++ funcoes-strings.php
@@ -8,14 +8,39 @@
 
 <body>
     <?php
-    // ceil($numero) -> arredonda o valor para cima
-    // floor($numero) -> arredonda para baixo
-    // round($numero) -> arredonda o valor com base nas casas decimais
-    // rond() -> gera um inteiro aleatorio
-    // sqrt($numero) -> retorna a raiz quadrada
 
+    $texto = 'curso completo de PHP';
 
+    //string to lower
+    echo $texto . '<br>';
+    echo strtolower($texto);
+    echo '<hr>';
 
+    //string to upper
+    echo $texto . '<br>';
+    echo strtoupper($texto);
+    echo '<hr>';
+
+    //upper case first
+    echo $texto . '<br>';
+    echo ucfirst($texto);
+    echo '<hr>';
+
+    //string length -> quantidade de caracteres
+    echo $texto . '<br>';
+    echo strlen($texto);
+    echo '<hr>';
+
+    //string replace
+    $texto_replace = 'O valor total foi de 22.5 reais.';
+    echo $texto . '<br>';
+    echo str_replace('.', ',', $texto_replace);
+    echo '<hr>';
+
+    //string length -> quantidade de caracteres
+    echo $texto . '<br>';
+    // curso completo de PHP
+    echo substr($texto, 0, 14) . ' ...';
 
 
 
