f225dfdcfa504b2dca15aa23c6194b86b9e35916
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-16 19:51:08 -0300

    array

---
+++ array_multidimensional.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+    // $lista_coisas = array();
+    $lista_coisas = [];
+
+    $lista_coisas['frutas'] = array(1 => 'Banana', 2 => 'Maçã', 3 =>  'Morango', 4 => 'Uva');
+    $lista_coisas['pessoas'] = [1 => 'João', 2 => 'José', 3 => 'Maria'];
+    $lista_coisas['idiomas'] = ['Português', 'Espanhol', 'Francês'];
+
+
+    echo '<pre>';
+    print_r($lista_coisas);
+    echo '</pre>';
+
+    echo '<hr>';
+    echo $lista_coisas['frutas'][3];
+    echo '<br>';
+    echo $lista_coisas['pessoas'][2];
+    echo '<br>';
+    echo $lista_coisas['idiomas'][2];
+    ?>
+
+
+
+
+</body>
+
+</html>
