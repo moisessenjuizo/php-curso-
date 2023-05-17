75398f2e15c4a445d2cba6e011c80cb2d91e19be
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-17 18:19:16 -0300

    do_while

---
+++ do_while.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+    <?php
+
+    $x = 1;
+
+    do {
+        echo "X = $x <br>";
+        echo "Entrou no Do $x <hr>";
+
+        $x++; // criterio de parada
+        // continue
+        // break
+    } while ($x < 9);
+
+
+
+
+
+    ?>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
