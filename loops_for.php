6b4c057615a8a5c8a5371194a63ddb512a756657
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-17 19:11:21 -0300

    loop_for

---
+++ loops_for.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+
+    <?php
+    // $x = 1;
+    // while ($x < 10) {
+    // }
+
+    // do {
+
+    // } while();
+
+    // for ($x = 1; $x <= 10; $x++) {
+    //     echo "$x <br>";
+    // }
+    // for ($x = 10; true; $x--) {
+    //     if ($x >= -1) {
+    //         break;
+    //     }
+    //     echo "$x <br>";
+    // }
+    for ($x = 10; $x >= 0; $x--) {
+        echo "$x <br>";
+    }
+
+
+
+
+
+
+    ?>
+</body>
+
+</html>
