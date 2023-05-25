f300b2070fcf2ac7cb24f56945a6106b0e3cdd2f
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-25 19:28:26 -0300

    login

---
+++ app_help_desk/valida_login.php
@@ -0,0 +1,57 @@
+<?php
+
+//variavel que verifica se a autenticação foi realizada
+$usuario_autenticado = false;
+
+
+//usuarios do sistema
+$usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+);
+// echo '<pre>';
+// print_r($usuarios_app);
+// echo '</pre>';
+
+foreach ($usuarios_app as $user) {
+    // echo 'Usuário app: ' . $user['email'] . ' / ' . $user['senha'];
+    // echo '<br>';
+    // echo 'Usuário form: ' . $_POST['email'] . ' / ' . $_POST['senha'];
+
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+        $usuario_autenticado = true;
+    }
+
+    if ($usuario_autenticado) {
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
+    echo '<hr>';
+
+
+
+
+    // echo $_POST['email'];
+    // echo '<br>';
+    // echo $_POST['senha'];
+    // echo '<br>';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// print_r($_POST);
+// echo '<br>';
+// echo $_POST['email'];
+// echo '<br>';
+// echo $_POST['senha'];
