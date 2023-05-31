18b1317658c02050f6da97ce88e7e9c4c93fd1d2
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-05-31 17:32:37 -0300

    linkando as pagins

--- app_help_desk/abrir_chamado.php
+++ app_help_desk/abrir_chamado.php
@@ -1,10 +1,4 @@
-<?php
-session_start();
-if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-  header('Location: index.php?login=erro2');
-}
-
-?>
+<?php require_once "validador_acesso.php" ?>
 
 <html>
 
@@ -68,11 +62,15 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
 
                   <div class="row mt-5">
                     <div class="col-6">
-                      <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+
+                      <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
+
                     </div>
 
                     <div class="col-6">
-                      <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+
+                      <a class="btn btn-lg btn-info btn-block" type="submit">Abrir</a>
+
                     </div>
                   </div>
                 </form>
