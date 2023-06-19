78da16bba23c4f5344f37a79ed503e47e8a0a65b
moisessenjuizo <63744932+moisessenjuizo@users.noreply.github.com>
2023-06-19 17:44:04 -0300

    array

--- aulas/01-array-while.php
+++ aulas/01-array-while.php
@@ -10,19 +10,37 @@
     <?php
     // Crie um script capaz de produzir atráves de um laço de repetição, um array com 6 elementos numéricos aleatórios entre 1 e 60, simulando o funcionamento do sorteio da mega sena. Atente-se ao fato de que os números contidos dentro do array não podem ser repetidos.
 
-    $megaSena = array();
-    array_push($megaSena, mt_rand(1, 60));
-    //print_r($megaSena);
-    //echo '<hr>';
-    for ($idx = 0; $idx < 6; $idx++) {
-        $numeroR = mt_rand(1, 60);
-        $arraypush = array_push($megaSena, $numeroR);
-        echo $megaSena[$idx];
-        echo '<br>';
+    // $megaSena = array();
+    // array_push($megaSena, mt_rand(1, 60));
+    // //print_r($megaSena);
+    // //echo '<hr>';
+    // for ($idx = 0; $idx < 6; $idx++) {
+    //     $numeroR = mt_rand(1, 60);
+    //     $arraypush = array_push($megaSena, $numeroR);
+    //     echo $megaSena[$idx];
+    //     echo '<br>';
+    // }
+    // //print_r($megaSena);
+
+
+
+
+
+    //Gere uma string aleatória usando "$ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" e use strlen() para saber o tamanho da string. E faça um loop for e dentro use rand(0, $str_len - 1).
+
+
+    function random_str_generator($len_of_gen_str)
+    {
+        $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        $var_size = strlen($chars);
+        echo "Random string = ";
+        for ($x = 0; $x < $len_of_gen_str; $x++) {
+            $random_str = $chars[rand(0, $var_size - 1)];
+            echo $random_str;
+        }
+        echo "\n";
     }
-    //print_r($megaSena);
-
-
+    random_str_generator(8)
 
 
 
